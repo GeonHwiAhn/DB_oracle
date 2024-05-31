3e4b859a7a559fe791752bea2d524eb5d0407243
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-05-31 12:51:10 +0900

    function 실습 코드

--- SQL_코드/02_함수_실습.sql
+++ SQL_코드/02_함수_실습.sql
@@ -56,7 +56,7 @@ WHERE PHONE IS NOT NULL;
 
 --3. 주민등록번호의 생년월일 추출
 -->EMP_NAME, EMP_NO AS 생년월일
-SELECT EMP_NAME, EMP_NO, SUBSTR (EMP_NO, 1,6) AS "생년월일"
+SELECT EMP_NAME, EMP_NO, SUBSTR (EMP_NO, 1, 6) AS "생년월일"
 FROM EMPLOYEE;
 
 
@@ -122,7 +122,7 @@ FROM EMPLOYEE;
 
 --2. 부서별 평균 급여 조회
 -->AS 부서, AS 평균급여
-SELEC AVG(SALARY) AS "평균급여"
+SELECT AVG(SALARY) AS "평균급여"
 FROM EMPLOYEE;
 
 SELECT DEPT_CODE AS "부서", AVG(SALARY) AS "평균급여"
@@ -146,6 +146,40 @@ ORA-00937: not a single-group group function
 SELECT AVG(SALARY) AS "D5부서 평균급여"
 FROM EMPLOYEE
 WHERE DEPT_CODE = 'D5';
+
+
+/****** REPLACE 활용 실습 ******/
+
+--1. 이메일 주소에서 모든 점(.)을 하이픈(-) 대체
+-->EMP_ID, EMAIL, AS 수정된 이메일
+SELECT EMP_ID, EMAIL, REPLACE (EMAIL, '.' , '-') AS "수정된 이메일"
+FROM EMPLOYEE;
+--2. 주민번호에서 모든 하이픈(-)을 빈 문자열로 대체
+--. EMP_NAME, EMP_NO ,AS 수정된주민번호
+SELECT EMP_NAME, EMP_NO, REPLACE (EMP_NO, '-', ' ') AS "수정된주민번호"
+FROM EMPLOYEE;
+--3. 이름에서 '김'을 KIM으로 대체
+--> EMP_NAME, AS 수정된이름
+SELECT EMP_NAME, REPLACE (EMP_NAME, '김', 'KIM') AS "수정된이름"
+FROM EMPLOYEE;
+
+--4. 직무코드에서 J를 JOB으로 대체
+-->EMP_ID, JOB_CODE, 수정된 직무코드
+SELECT JOB_CODE, REPLACE (JOB_CODE, 'J', 'JOB') AS "수정된직무코드"
+FROM EMPLOYEE;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
